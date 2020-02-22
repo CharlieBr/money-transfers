c30826d3233a6cc89cc02d9bcb1b952f792626c4
CharlieBr <kasia_glab@wp.pl>
2020-02-22 23:02:10 +0100

    Introduce basic model for account

--- src/main/java/com/kg/money/transfers/model/Account.java
+++ src/main/java/com/kg/money/transfers/model/Account.java
@@ -1,5 +1,17 @@
 package com.kg.money.transfers.model;
 
-public class Account {
+import java.math.BigDecimal;
 
+public class Account {
+    private final String id;
+    private final BigDecimal value;
+
+    public Account(final String id, final BigDecimal value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public String getId() {
+        return this.id;
+    }
 }

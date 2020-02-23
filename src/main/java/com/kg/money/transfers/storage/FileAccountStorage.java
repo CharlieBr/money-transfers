efcb1898105c5b941ed03dd7ba980bb88bdf45a1
CharlieBr <kasia_glab@wp.pl>
2020-02-23 19:07:47 +0100

    Accounts stored in map instead of set

--- src/main/java/com/kg/money/transfers/storage/FileAccountStorage.java
+++ src/main/java/com/kg/money/transfers/storage/FileAccountStorage.java
@@ -1,19 +1,24 @@
 package com.kg.money.transfers.storage;
 
+import static java.util.stream.Collectors.toMap;
+
+import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
+import java.util.function.Function;
 
 import com.kg.money.transfers.model.Account;
 
 public class FileAccountStorage implements AccountStorage {
-    private final Set<Account> accounts;
+    private final Map<String, Account> accounts;
 
     public FileAccountStorage(final String filename) {
-        this.accounts = AccountStorageFileParser.parseAccountsFromFile(filename);
+        this.accounts = AccountStorageFileParser.parseAccountsFromFile(filename)
+                .stream()
+                .collect(toMap(Account::getId, Function.identity()));
     }
 
     @Override
     public Optional<Account> getById(final String id) {
-        return Optional.empty();
+        return Optional.ofNullable(this.accounts.get(id));
     }
 }
